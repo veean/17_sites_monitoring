f69c292de3d0f67617bc919ab84e5d3ac148ff62
veean <nersesowm@gmail.com>
2017-01-08 11:26:20 +0400

    fix variable names

--- check_sites_health.py
+++ check_sites_health.py
@@ -21,18 +21,18 @@ def is_server_respond_with_200(url):
     return requests.get(url).status_code == ok_status
 
 
-def get_domain_expiration_date(domain_name):
-    info_request = whois.whois(domain_name)  # can be a list of datetimes
+def get_domain_expiration_date_or_datetime_list(domain_name):
+    info_request = whois.whois(domain_name)
     return info_request.expiration_date
 
 
-def input_wrapper():
+def get_path_argument():
     parser = argparse.ArgumentParser(description='Checking availability of urls list')
     parser.add_argument('file', default='urls.txt', type=str, help='file with urls')
     return parser.parse_args().file
 
 
-def paid_at_least_a_month(expiration_date):
+def is_domain_paid_at_least_a_month(expiration_date):
     expiration_prediction = datetime.today() + timedelta(days=DAYS_TO_EXPIRE)
     if type(expiration_date) is list:
         for possible_unit in expiration_date:
@@ -41,22 +41,22 @@ def paid_at_least_a_month(expiration_date):
         return expiration_date > expiration_prediction
 
 
-def complex_check(url_):
+def get_avaliability_and_expiration(url_):
     if is_server_respond_with_200(url_):
         print("{} has status OK and ... ".format(url_))
     else:
         print("Something wrong with {} and ...".format(url_))
 
-    if paid_at_least_a_month(get_domain_expiration_date(url_)):
+    if is_domain_paid_at_least_a_month(get_domain_expiration_date_or_datetime_list(url_)):
         print('domain name paid at least a month forward . \n ')
     else:
         print("domain name expires less then a month! \n ")
 
 
 if __name__ == '__main__':
-    list_to_check = load_urls4check(input_wrapper())
+    list_to_check = load_urls4check(get_path_argument())
     if list_to_check:
         for item in list_to_check:
-            complex_check(item)
+            get_avaliability_and_expiration(item)
     else:
         print('None of urls in specified file!')
